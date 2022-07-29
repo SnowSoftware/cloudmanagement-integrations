f4cac01302961d4555dd37fc8e1b48a8a80acb14
Scott Bulloch <scott.bulloch@snowsoftware.com>
2022-07-29 15:46:01 -0400

    Veeam 11 workflows

---
+++ Veeam/Add_to_Specific_Backup_Job-Command_Workflow.ps1
@@ -0,0 +1,103 @@
+<#
+Requirements
+- In Snow Commander the Advanced Property “embotics.workflow.script.credentials” must be set to true.
+- In some cases the Execution Policy on the Commander Server must be set to “Unrestricted”
+- Commander 8.10.6 or greater
+- Minimum Veeam Backup and Recovery Console 11 on the Commander server.
+#>
+
+#Veeam Configuration
+$VBRServer = "x.x.x.x"
+$VBRport = "9392" #Default port is 9392
+$vBRusername = (Get-Item Env:SELECTED_CREDENTIALS_USERNAME).value;
+$vBRpassword = (Get-Item Env:SELECTED_CREDENTIALS_PASSWORD).value
+$Modules = "Veeam.Backup.PowerShell"
+$BypassCert = "Yes"
+$Jobname = "Monthly"
+$VMDetails = "#{target.deployedName}"
+
+#Check to make sure required information is present
+if (!($vBRusername) -or !($vBRpassword) -or !($VBRServer) -or !($VBRport) -or !($Jobname) -or !($BypassCert)) {
+    Write-error "Please provide required information to connect to Veeam"
+    Exit 1
+} 
+
+$ErrorActionPreference = "STOP"
+
+#Check for PS Module
+ForEach ($module in $modules) {
+    if (Get-Module -ListAvailable -Name $Module) {
+        Import-Module $module -WarningAction Ignore
+        Write-Debug "Module $Module is installed."
+    } 
+    else {
+        Write-Error "Module $module does not appear to be installed, Please install and run again."
+        Exit 1
+    }
+}
+
+#Bypass unsigned Certificates
+if ($BypassCert -eq "yes") {
+    Write-host "- Ignoring invalid Certificate" -ForegroundColor Green
+    add-type @"
+   using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+    [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+    [Net.ServicePointManager]::Expect100Continue = $true
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+}
+
+#Connect to vBRserver
+$secpassword = $vBRpassword | ConvertTo-SecureString -AsPlainText -Force
+$vBRcred = New-Object System.Management.Automation.PSCredential($vBRusername, $secpassword)
+Connect-VBRServer -Server $VBRServer -Port $VBRport -Credential $vBRCred
+$vBRSession = Get-VBRServerSession
+if ($vBRSession.Server -eq $VBRServer) {
+    Write-host "Connected to Veeam Server at address: $VBRServer"
+} 
+if (!$vBRSession.Server) {
+    Write-host "Not Connected to $VBRServer, Please contact your Administrator"
+    Exit 1
+}
+
+#Check if job exists
+$vbrJobs = (Get-VBRJob).name
+if ($vbrJobs -notcontains $Jobname) {
+    Write-host "Backup job '$jobname' doesn't exist on the server, please contact your administrator"
+    Disconnect-VBRServer
+    Exit 1
+}
+    
+#Add VM to Backup Job
+$vmEntity = Find-VBRViEntity -name $VMDetails
+IF (!$vmEntity) {
+    Write-host "No vm found with the name: $VMDetails" -foreground "red"
+    Disconnect-VBRServer
+    Exit 1
+}
+$AddResult = Add-VBRViJobObject -Job $Jobname -Entities $vmEntity -Force -WarningAction Ignore
+
+#Check job for added VM
+$jobobject = $jobname | Get-VBRJobObject | where { $_.name -match $vmEntity.name }
+If ($jobobject.Name -eq $VMDetails) {
+    Write-host "'$VMDetails' succesfully added to backup job: $jobname"
+    Disconnect-VBRServer
+}
+Elseif (!$jobobject) {
+    Write-Error "'$VMDetails' succesfully added to backup job: $jobname"
+    Disconnect-VBRServer
+    Exit 1
+}
+Else {
+    Write-Error "Something has gone wrong, Please contact your system administrator"
+    Disconnect-VBRServer
+}
+
--- Veeam/readme.md
+++ Veeam/readme.md
@@ -4,6 +4,7 @@ Modules that allow a user to add a vm to a backup job as part of deployment or a
 
 ### Requirements
  - Requires Veeam Backup and Recovery Console on the Commander server.
+ - Veeam Backup and Recovery 11 or higher.
  - Commander 8.9.0 or higher
  - Advanced property "embotics.workflow.script.credentials" must be set to "true"
 
@@ -12,14 +13,26 @@ Modules that allow a user to add a vm to a backup job as part of deployment or a
  * Copy the contents of Dynamic_List_Backup_Selection.ps1 into a Dynamic list Form Configuration
  * Select your Veeam credential in the credentials list
  * Set the Executable to powershell.exe then run a test to verify it's operation
-### Completion modules
-*  
+
+### Command Module for quick 'add to backup' Action in portal with no Approval
+ * Allows users to Add VM to backup Job witout approval
+ * Import the Workflow 'Add_to_Specific_Backup_Job-Command_Workflow.json'
+ * Edit the newly Imported Workflow, Select your Veeam credential in the credentials list. 
+ * Add the IP address or FQDN of your Veeam Server and Specify the Backup Job Name. 
+ * edit the workflow name if exposing it in the portal with the jobname. 
  
-### Setup Steps
-1. 
-2. 
-3. 
-4. 
+### Completion Module "Add to Backup"
+ * Import the Completion Module 'Add_to_Veeam_Backup_Job-Completion_Module.json'
+ * Edit the newly Imported Module, modify the execute script step and Select your Veeam credential in the credentials list. 
+ * Configure the Input Variables the IP address or FQDN of your Veeam Server, Port and Specify the Backup Job Name. 
+ Note: You can use the input from the dynamic list to allow the user to select from the current Job list in Veeam
+ 
+### Command workflow for quick 'Backup Now' Action in portal with no Approval
+ * Allows users to run a quick Backup action to backup Job witout approval
+ * Import the Workflow 'Run_Quick_Backup-Command_Workflow.json'
+ * Edit the newly Imported Workflow, Select your Veeam credential in the credentials list. 
+ * Add the IP address or FQDN of your Veeam Server. 
+ 
 ________________
 
 *Currently being migrated from [Embotics Git](https://github.com/Embotics)*
